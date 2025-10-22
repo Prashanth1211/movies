625ceb204df64ad5034aba020bcb556f6c527583
Prashanth R <rprashanth1211@gmail.com>
2025-10-23 10:44:16 +1100

    Update the API

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=5c77cdec`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=5c77cdec`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
